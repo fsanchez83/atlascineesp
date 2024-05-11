71be0a8d4a78c82b1884c16c74056b70231e1b83
Faustino <ccbaxterwilder@gmail.com>
2024-05-11 21:07:13 +0200

    Primera version completa

---
+++ datasetEnriquecido.py
@@ -0,0 +1,115 @@
+'''
+Created on 30 abr. 2020
+
+@author: fsanchez
+'''
+# -*- coding: utf-8 -*-
+
+import tmdbsimple as tmdb
+import pandas as pd
+import yaml
+from yaml.loader import SafeLoader
+import sys
+import os.path
+
+with open('secrets.cfg') as f:
+    secrets = yaml.load(f, Loader=SafeLoader)
+
+tmdb.API_KEY = secrets['TMDB']['API_KEY']
+
+# Leer la lista básica de pelis
+
+lista_con_id = pd.read_csv('movie_data_parte1.csv', sep=';')
+
+Atributos_peli = ["tmdb_id", "Titulo", "Popularidad", 'Rating', 'Fecha', 'Duracion', 'Pais', 'Idioma',
+                  'Presupuesto', 'Ganancia', 'Generos', 'Director', 'Director_genre',
+                  'Casting', 'Guion', 'Montaje', 'DOP', 'Resumen', 'Productoras',
+                  'Paises productores']
+df_films = pd.DataFrame(columns=Atributos_peli)
+
+search = tmdb.Search()
+
+dataset_inicial = pd.read_csv('dataset_pelis.csv', sep=';')
+
+dataset_nuevas = pd.merge(dataset_inicial,lista_con_id,left_on=['tmdb_id'],
+                          right_on=['id'],how="outer",indicator=True)
+dataset_nuevas = dataset_nuevas[dataset_nuevas['_merge'] == 'right_only']
+dataset_nuevas_pelis = dataset_nuevas.copy()
+dataset_nuevas_pelis = dataset_nuevas_pelis.rename(columns={"Name_x": "Name","tmdb_type_y":"tmdb_type","Rating_y": "Rating"})
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pelis_con_error = []
+
+print("")
+print("EXTRAE DATOS NUEVAS PELIS:")
+
+for index, row in dataset_nuevas_pelis.iterrows():
+    print(str(index)+": "+row['original_title'])
+    tmdb_id = row['id']
+    try:
+        movie = tmdb.Movies(tmdb_id)
+        movieInfo = movie.info()
+        id_peli = tmdb_id
+        titulo = movieInfo['title']
+        popularidad = movieInfo['popularity']
+        rating = movieInfo['vote_average']
+        fecha = movieInfo['release_date']
+        duracion = movieInfo['runtime']
+        idioma = movieInfo['original_language']
+
+        presupuesto = movieInfo['budget']
+        ganancia = movieInfo['revenue']
+        resumen = movieInfo['overview'].replace('\n', ' ').replace('\r', ' ')
+        generos = []
+        emp_productoras = []
+        paises_productores = []
+        pais = movieInfo['origin_country']
+
+        for dic in movieInfo['genres']:
+            generos.append(dic['name'])
+        for dic in movieInfo['production_companies']:
+            emp_productoras.append(dic['name']+"--"+dic['origin_country'])
+        for dic in movieInfo['production_countries']:
+            paises_productores.append(dic['name'])
+
+        director = []
+        director_genre = []
+        guion = []
+        montaje = []
+        dop = []
+        casting = []
+
+        creditos = movie.credits()
+        for dic in creditos['crew']:
+            if dic['job'] == 'Director':
+                director.append(dic['name'])
+                director_genre.append(dic['gender'])
+            if dic['job'] == 'Screenplay':
+                guion.append(dic['name'])
+            if dic['job'] == 'Editor':
+                montaje.append(dic['name'])
+            if dic['job'] == 'Director of Photography':
+                dop.append(dic['name'])
+
+        for dic in creditos['cast']:
+            casting.append(dic['name'])
+
+        lista_peli = [tmdb_id, titulo, popularidad, rating, fecha, duracion, pais, idioma,
+                      presupuesto, ganancia, generos, director, director_genre,
+                      casting, guion, montaje, dop, resumen, emp_productoras,
+                      paises_productores]
+
+
+        df_tamanio = len(dataset_inicial)
+        dataset_inicial.loc[df_tamanio] = lista_peli
+
+    except:
+        print("Error en el nombre")
+        pelis_con_error.append(row['original_title'])
+
+dataset_inicial.to_csv('dataset_pelis.csv', sep=';', index=False)
+
+df_pelis_con_error = pd.DataFrame(pelis_con_error, columns=['titulo'])
+df_pelis_con_error.to_csv('pelis_con_error.csv', sep=';', index=False)
+
+print('FIN')
---
+++ recuperaPeliculas.py
@@ -0,0 +1,55 @@
+import requests
+import csv
+import yaml
+from yaml.loader import SafeLoader
+import sys
+
+with open('secrets.cfg') as f:
+    secrets = yaml.load(f, Loader=SafeLoader)
+
+with open('config.cfg') as c:
+    dataConfig = yaml.load(c, Loader=SafeLoader)
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer "+secrets['TMDB']['BEARER']
+}
+
+date_in = dataConfig['Fechas']['date_in']
+date_fin = dataConfig['Fechas']['date_fin']
+
+def get_movie_data(page, date_in, date_fin):
+    url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false" \
+          f"&include_video=true&page={page}&primary_release_date.gte=" \
+          f"{date_in}&primary_release_date.lte={date_fin}&sort_by=primary_release_date" \
+          f".asc&with_origin_country=ES"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data.get('results', [])
+
+def save_to_csv(movie_data, filename):
+    with open(filename, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=[
+            "id", "genre_ids", "original_language", "original_title", "overview",
+            "popularity", "poster_path", "release_date", "title", "video",
+            "vote_average", "vote_count", "backdrop_path", "adult"
+        ], delimiter=';')
+        if file.tell() == 0:  # Verifica si el archivo está vacío
+            writer.writeheader()
+        for movie in movie_data:
+            # Convertimos los géneros a una cadena separada por comas
+            movie['genre_ids'] = ','.join(map(str, movie['genre_ids']))
+            writer.writerow(movie)
+
+def main():
+    filename = dataConfig['Resultados']['filename_lista']
+    #for page in range(1, 3):  # Itera sobre las páginas de la API
+    for page in range(1, 501):  # Itera sobre las páginas de la API
+        movie_data = get_movie_data(page,date_in,date_fin)
+        if movie_data:
+            save_to_csv(movie_data, filename)
+            #print(f"Guardando datos de la página {page} en {filename}")
+            print(page)
+
+if __name__ == "__main__":
+    main()
